596f02d77776e71a32d103b527533164cb82affe
fanjinxiang <fanjinxiang@odianyun.com>
2020-03-21 16:08:18 +0800

    添加reactive访问操作

--- src/main/java/com/springaction/demo/DemoApplication.java
+++ src/main/java/com/springaction/demo/DemoApplication.java
@@ -2,13 +2,11 @@ package com.springaction.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
